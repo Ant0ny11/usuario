8cb34573393648fb3359ac838016a2c07735ec10
Felipe Antony <feantony17@gmail.com>
2025-12-29 14:09:53 -0300

    adicionados metodos get, delete e login

--- src/main/java/com/javanauta/usuario/infraestructure/config/repository/EnderecoRepository.java
+++ src/main/java/com/javanauta/usuario/infraestructure/config/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.javanauta.usuario.infraestructure.config.repository;
 
-import com.javanauta.aprendendospring.infraestructure.entity.Endereco;
+
+import com.javanauta.usuario.infraestructure.config.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/javanauta/usuario/infraestructure/config/repository/TelefoneRepository.java
+++ src/main/java/com/javanauta/usuario/infraestructure/config/repository/TelefoneRepository.java
@@ -1,6 +1,7 @@
 package com.javanauta.usuario.infraestructure.config.repository;
 
-import com.javanauta.aprendendospring.infraestructure.entity.Telefone;
+
+import com.javanauta.usuario.infraestructure.config.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
